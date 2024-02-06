d9c7ce73d5016c1f4fbf8e9b7b3cb6abf52cf6fc
felicebloom <119356152+felicebloom@users.noreply.github.com>
2024-02-05 22:28:31 -0500

    first commit

---
+++ airbnb/js/main.js
@@ -0,0 +1,13 @@
+//console.log("js has been loaded");
+
+const menuBtn = document.getElementById('menuBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+const closeBtn = document.getElementById('closeBtn');
+
+menuBtn.addEventListener('click', function() {
+    mobileMenu.classList.add('active');
+});
+
+closeBtn.addEventListener('click', function() {
+    mobileMenu.classList.remove('active');
+});
